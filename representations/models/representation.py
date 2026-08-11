e99fe4a6f0b4e9d7bb17f44d6a55775eb9acaed1
Wilson <1117116@est.intec.edu.do>
2026-08-11 13:48:28 -0400

    Refactor SignService and WordService update methods to accept sign_name and word_name as parameters; update create methods to include user information.

--- representations/models/representation.py
+++ representations/models/representation.py
@@ -4,7 +4,7 @@ import uuid
 # Create your models here.
 class Representation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    sign_id = ''
+    sign_id = models.CharField('sign=sing', )
     create_by = ''
     extension = ''
     url = ''
