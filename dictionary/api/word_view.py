899a4826d689c6f6a947c1eb4733bdcdc9e02b04
Uriel020 <urielrre@gmail.com>
2026-08-14 23:09:10 -0400

    Refactor API and serializers for sign and word views
    - Update sign_view.py and word_view.py to improve code structure.
    - Revise sign_serializer.py and word_serializer.py for better performance.
    - Clean up user_services.py by removing unused code.

--- dictionary/api/word_view.py
+++ dictionary/api/word_view.py
@@ -13,11 +13,11 @@ class WordView():
         word_service = WordService()
         try:
             words = word_service.find_all_active()
-            serializer = WordSerializer(words, words=True)
+            serializer = WordSerializer(words, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except ValueError as e:
             return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    
+
     @staticmethod
     @api_view(["GET"])
     def get(request):
@@ -25,13 +25,13 @@ class WordView():
         word_name = request.query_params.get("word_name")
         if not word_name:
             return Response(
-                {"detail": "word_name query parameter is required "},
+                {"detail": "word_name query parameter is required"},
                 status=status.HTTP_400_BAD_REQUEST,
             )
         try:
             word = word_service.find_by_word_name(word_name)
-            serializer = WordSerializer(word, many=True)
+            serializer = WordSerializer(word)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        
+
         except ValueError as e:
             return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
